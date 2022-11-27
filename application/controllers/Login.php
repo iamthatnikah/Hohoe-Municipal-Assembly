db680705f20f754198d09421792c45901ffe70bc
iamthatnikah <kobbyaryeetey@gmail.com>
2022-11-27 22:18:45 +0000

    Login Controller and Model Added Successfully

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -31,4 +31,28 @@ class Login extends CI_Controller {
 		//$this->load->view('admin/includes/footer');
 		//$this->load->view('dashboard');
 	}
+	public function add_dispatch(){
+		// CI form validation 
+		$this->form_validation->set_rules('reg_no', 'Reg No', 'required');
+		$this->form_validation->set_rules('drp', 'Date Received', 'required');
+		//$this->form_validation->set_rules('added_at', 'When Added', 'required');
+		//$this->form_validation->set_rules('mod', 'Mode of Dispatch', 'required');
+
+		// check pass validation
+		if($this->form_validation->run() == FALSE){
+			$this->index();
+		}else{
+			// pass values to filelist_model  
+			$returnInsert = $this->login_model->create_dispatch();
+
+			if($returnInsert){
+				$this->session->set_flashdata('success', 'Login Successfully');
+				 redirect('dashboard');
+			}else{
+				$this->session->set_flashdata('error', 'Something went wrong: try again');
+				 redirect('login');
+			}
+		}
+		
+	}
 }
--- application/models/login_model.php
+++ application/models/login_model.php
@@ -4,33 +4,20 @@
       $this->load->database();
     }
 
-    // Get all dispatch from db
-    public function get_all_dispatch(){
-      $this->db->order_by('id', 'DESC');
-      $query = $this->db->get('dispatch');
-      return $query->result_array();
-    }
 
-    // create dispatch
-    public function create_dispatch(){
+    // login dashboard
+   /* public function login_dashboard($id = FALSE){
       $data = array(
-        'reg_no' => $this->input->post('reg_no'),
-        'drp' => date('d-m-y h:m:s'),
-        'date_dispatch' => date('d-m-y h:m:s'),
-        'subject' => $this->input->post('subject'),
-        'whom_sent' => $this->input->post('whom_sent'),
-        'ref_no' => $this->input->post('ref_no'),
-        'folio' => $this->input->post('folio'),
-        'addressee' => $this->input->post('addressee'),
-        'added_at' => date('d-m-y h:m:s'),
-        'mod' => $this->input->post('mod')
+        'email' => $this->input->post('reg_no'),
+        'password' => $this->input->post('reg_no')
+       
       );
 
       return $this->db->insert('dispatch', $data);
-    }
+    }*/
 
-    // get dispatch by ID
-    public function get_dispatch_id($id = FALSE){
+    // get User by ID
+    public function login_dashboard($id = FALSE){
         if($id === FALSE){
             $query = $this->db->get('dispatch');
             return $query->result_array();
@@ -39,23 +26,6 @@
       return $query->row_array();
     }
 
-    // update dispatch
-    public function update_dispatch(){
-      $data = array(
-        'reg_no' => $this->input->post('reg_no'),
-        'drp' => $this->input->post('drp'),
-        'date_dispatch' => $this->input->post('date_dispatch'),
-        'subject' => $this->input->post('subject'),
-        'whom_sent' => $this->input->post('whom_sent'),
-        'ref_no' => $this->input->post('ref_no'),
-        'folio' => $this->input->post('folio'),
-        'addressee' => $this->input->post('addressee'),
-        'added_at' => $this->input->post('added_at'),
-        'mod' => $this->input->post('mod')
-      );
-      $this->db->where('id', $this->input->post('id'));
-     return $this->db->update('dispatch', $data);
-    }
 
     // delete dispatch
     public function delete_dispatch($id){
