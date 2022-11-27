2792ab9ceeb557047250afb8df26907e7b3d2202
iamthatnikah <kobbyaryeetey@gmail.com>
2022-11-27 19:29:13 +0000

    Added a Login & the Dashboard pages Successfully

---
+++ application/controllers/Login.php
@@ -0,0 +1,34 @@
+<?php
+//Main Dashboard
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/userguide3/general/urls.html
+	 */
+	public function index()
+	{
+		//loading the dashboard from the views
+		$this->load->view('login');
+
+		$this->load->view('includes/head');
+		// $this->load->view('admin/includes/navbar');
+		// $this->load->view('admin/includes/sidebar');
+		
+		//$this->load->view('admin/includes/footer');
+		//$this->load->view('dashboard');
+	}
+}
---
+++ application/models/login_model.php
@@ -0,0 +1,66 @@
+<?php
+  class Login_model extends CI_Model{
+    public function __construct(){
+      $this->load->database();
+    }
+
+    // Get all dispatch from db
+    public function get_all_dispatch(){
+      $this->db->order_by('id', 'DESC');
+      $query = $this->db->get('dispatch');
+      return $query->result_array();
+    }
+
+    // create dispatch
+    public function create_dispatch(){
+      $data = array(
+        'reg_no' => $this->input->post('reg_no'),
+        'drp' => date('d-m-y h:m:s'),
+        'date_dispatch' => date('d-m-y h:m:s'),
+        'subject' => $this->input->post('subject'),
+        'whom_sent' => $this->input->post('whom_sent'),
+        'ref_no' => $this->input->post('ref_no'),
+        'folio' => $this->input->post('folio'),
+        'addressee' => $this->input->post('addressee'),
+        'added_at' => date('d-m-y h:m:s'),
+        'mod' => $this->input->post('mod')
+      );
+
+      return $this->db->insert('dispatch', $data);
+    }
+
+    // get dispatch by ID
+    public function get_dispatch_id($id = FALSE){
+        if($id === FALSE){
+            $query = $this->db->get('dispatch');
+            return $query->result_array();
+        }
+      $query = $this->db->get_where('dispatch', array('id' => $id));
+      return $query->row_array();
+    }
+
+    // update dispatch
+    public function update_dispatch(){
+      $data = array(
+        'reg_no' => $this->input->post('reg_no'),
+        'drp' => $this->input->post('drp'),
+        'date_dispatch' => $this->input->post('date_dispatch'),
+        'subject' => $this->input->post('subject'),
+        'whom_sent' => $this->input->post('whom_sent'),
+        'ref_no' => $this->input->post('ref_no'),
+        'folio' => $this->input->post('folio'),
+        'addressee' => $this->input->post('addressee'),
+        'added_at' => $this->input->post('added_at'),
+        'mod' => $this->input->post('mod')
+      );
+      $this->db->where('id', $this->input->post('id'));
+     return $this->db->update('dispatch', $data);
+    }
+
+    // delete dispatch
+    public function delete_dispatch($id){
+      $this->db->where('id', $id);
+      $this->db->delete('dispatch');
+    }
+
+  }
