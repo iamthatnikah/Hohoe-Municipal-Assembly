d7f57eff44e8cfeed1269b7f07a5897a076b3178
iamthatnikah <kobbyaryeetey@hotmail.com>
2024-09-03 01:32:18 +0000

    Updates

--- app/views/includes/footer.php
+++ app/views/includes/footer.php
@@ -8,7 +8,7 @@
       <!-- You can delete the links only if you purchased the pro version. -->
       <!-- Licensing information: https://bootstrapmade.com/license/ -->
       <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-      Designed by <a href="https://github.com/">iamthatnikah</a>
+      Designed by <a href="https://iamthatnikah.github.io/portfolio/">iamthatnikah</a>
     </div>
   </footer><!-- End Footer -->
 
